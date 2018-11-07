90bf385c5c403aab0d09bde16f8a4a47b68ea253
Aaron Austin <aaron@aaronaustin.com>
2018-11-07 18:34:54 -0500

    update metabox styles

--- custom-admin-theme.php
+++ custom-admin-theme.php
@@ -1,7 +1,7 @@
 <?php
 /*Plugin Name: Awesome Custom Admin Theme by Aaron Austin
 Description: This makes things look badass.
-Version: 1.0.3
+Version: 1.2.1
 License: GPLv2
 GitHub Plugin URI: https://github.com/aaronaustin/custom-admin-theme
 */
